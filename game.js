1f78a939a405627e1c9b96633fcea63ed7b12a87
Gav <g_eyquem@hotmail.com>
2021-09-28 14:23:21 +0100

    Further variables and some deletions

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
 const qwerty = document.querySelector('#qwerty');
 const phrase = document.querySelector('#phrase');
 const missed = 0;
-
+const startBtn = document.querySelector('btn__reset');
+const overlay = document.querySelector('#overlay');
 
 
 btn__reset.addEventListener('click', () => {
