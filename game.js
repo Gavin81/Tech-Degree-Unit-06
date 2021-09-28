5dc29f038447dc710318d91557ea37945cea319f
Gav <g_eyquem@hotmail.com>
2021-09-28 14:09:42 +0100

    Adding variables and addEventListener

--- game.js
+++ game.js
@@ -1,3 +1,9 @@
-const something =
-const
-const
+const qwerty = document.querySelector('#qwerty');
+const phrase = document.querySelector('#phrase');
+const missed = 0;
+
+
+
+btn__reset.addEventListener('click', () => {
+
+});
