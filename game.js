60c908221c4da7a40d606cbd7aa7b4f28affb9fb
Gav <g_eyquem@hotmail.com>
2021-12-01 15:29:52 +0000

    changed the code

--- game.js
+++ game.js
@@ -1,10 +1,10 @@
 const qwerty = document.querySelector('#qwerty');
 const phrase = document.querySelector('#phrase');
-const missed = 0;
+let missed = 0;
 const startBtn = document.querySelector('btn__reset');
 const overlay = document.querySelector('#overlay');
 
 
 btn__reset.addEventListener('click', () => {
-
+  overlay.style.display = hide;
 });
