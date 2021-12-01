b3257322f0f5ec57ac58099640932254a68b886f
Gav <g_eyquem@hotmail.com>
2021-12-01 16:08:55 +0000

    Update game.js
    Changed selector type

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 const qwerty = document.querySelector('#qwerty');
 const phrase = document.querySelector('#phrase');
 let missed = 0;
-const startBtn = document.querySelector('btn__reset');
+const startBtn = document.getElementsByClassName('btn__reset');
 const overlay = document.querySelector('#overlay');
 
 
