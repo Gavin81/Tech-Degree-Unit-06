7a9adcf49e6a408da3f338a86f7e49bc755e161d
Gav <g_eyquem@hotmail.com>
2021-12-10 16:28:21 +0000

    updated the code

--- game.js
+++ game.js
@@ -1,5 +1,6 @@
 const qwerty = document.querySelector('#qwerty');
 const phrase = document.querySelector('#phrase');
+const phrases = ['Reincarnation', 'Movies', 'Slapstick', 'Respect', 'Money', 'Time and effort'];
 let missed = 0;
 const startBtn = document.querySelector('.btn__reset');
 const overlay = document.querySelector('#overlay');
@@ -8,3 +9,15 @@ const overlay = document.querySelector('#overlay');
 startBtn.addEventListener('click', () => {
   document.getElementById('overlay').style.display = "none";
 });
+
+
+getRandomPhraseAsArray (arr) {
+
+}
+
+getRandomPhraseAsArray(phrases);
+
+
+addPhraseToDisplay (arr) {
+
+}
