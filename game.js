008a3d1814840db1e8cd3ddb8fd8e1b9ac6b7f49
Gavin81 <g_eyquem@hotmail.com>
2022-11-22 16:57:23 +0000

    Update game.js

--- game.js
+++ game.js
@@ -79,9 +79,9 @@ qwerty.addEventListener('click', (event) => {
     const letterFound = checkLetter(event.target);
     //missed++;
   }
-
-  if(checkLetter === null){
-
-  }
-
 });
+
+
+if(checkLetter === null){
+
+}
